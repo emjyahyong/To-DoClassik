8738011324dd80306a32be2d6bfb25d39a17a79c
Emjy <emjy.ahyong@gmail.com>
2025-04-18 22:13:57 +0400

    Just for streak

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+      <h1>Bienvenue sur To-DoClassik</h1>
+      <p v-show="show">Aucune tâche</p>
+      <button @click="add">Ajouter</button>
+      {{ cpunt }}
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+
+  </script>
+  
+  <style>
+  #app {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin-top: 50px;
+  }
+  </style>
+  
