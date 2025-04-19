dd2619ccd4d167954f039a0444bbc19fa0d05c36
Emjy <emjy.ahyong@gmail.com>
2025-04-19 21:52:11 +0400

    Add tache

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <template>
-    <div id="app">
-      <h1>Bienvenue sur To-DoClassik</h1>
-      <p v-show="show">Aucune tâche</p>
-      <button @click="add">Ajouter</button>
-      {{ cpunt }}
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
+  <div id="app">
+    <h1>Bienvenue sur To-DoClassik</h1>
+    <form action="" @submit.revent="add">
+      <label for="tache">Ajouter une tâche</label>
+      <input type="text" id="tache" v-model="textTache" />
+      <button>Ajouter</button>
+    </form>
+    <p v-if="!nbTaches >= 1">Aucune tâche</p>
+    <ul>
+      <li v-for="tache in taches">{{ tache.description }}</li>
+    </ul>
+  </div>
+</template>
 
-  </script>
-  
-  <style>
-  #app {
-    font-family: Arial, sans-serif;
-    text-align: center;
-    margin-top: 50px;
-  }
-  </style>
-  
+<script setup>
+import { ref } from "vue";
+const nbTaches = ref(0);
+const textTache = ref("");
+const tache = ref({
+  description: "",
+  completed: false,
+  date: new Date(),
+});
+const taches = ref([]);
+
+const add = (event) => {
+  event.preventDefault();
+  nbTaches.value++;
+  tache.value.description = textTache.value;
+  taches.value.push(tache.value);
+};  
+// const sortTaches = () => {
+//   taches.value.sort()
+// }
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
